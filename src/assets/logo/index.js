c48df74dc646d7f5db88d2f21f7aa9a0cb34413d
Julien <julien.robert@kciope.com>
2022-10-15 15:27:08 +0200

    set up

---
+++ src/assets/logo/index.js
@@ -0,0 +1,15 @@
+import { ReactComponent as Insta } from './instagram.svg'
+import { ReactComponent as Facebook } from './facebook.svg'
+import { ReactComponent as Youtube } from './lecture-de-youtube.svg'
+import { ReactComponent as Twitter } from './twitter.svg'
+import { ReactComponent as Vimeo } from './vimeo.svg'
+
+export const svgs = {
+  Insta,
+  Facebook,
+  Youtube,
+  Twitter,
+  Vimeo,
+}
+
+export const imgs = {}
